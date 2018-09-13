a674145b0fc5c13f074e4b3eedd614ed9099caa0
Oscar <oscar.braunert@kitchenstories.de>
2018-09-13 22:07:11 +0200

    add base combobox

---
+++ src/components/combobox/A11yCombobox.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    role="combobox"
+    aria-owns="vCbResultList"
+    aria-haspopup="listbox"
+    :aria-expanded="showsList"
+  >
+    <input
+      v-model="inputValue"
+      type="text"
+      name="cbInput"
+      role="searchbox"
+      aria-autocomplete="list"
+      aria-label="Input search term"
+      aria-controls="vCbResultList"
+      aria-activedescendant="@TODO"
+      aria-multiline="false"
+    >
+    <a11y-combobox-list
+      v-show="showsList"
+      role="listbox"
+    />
+  </div>
+</template>
+
+<script>
+import A11yComboboxList from './A11yComboxList.vue'
+
+export default {
+  components: {
+    A11yComboboxList
+  },
+  data () {
+    return {
+      inputValue: ''
+    }
+  },
+  computed: {
+    showsList () {
+      return this.inputValue
+    }
+  }
+}
+</script>
---
+++ src/components/combobox/A11yComboboxList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul id="vCbResultList">
+    <li
+      v-for="(item, index) in items"
+      :key="index"
+      :id="`vCbItem-${index}`"
+      role="option"
+    >
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
