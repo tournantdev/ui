9bd21165f0bc917aeda5b667fc86bbe668f2a7a5
Oscar <oscar.braunert@kitchenstories.de>
2018-09-14 04:07:28 +0200

    rmv old code

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,6 @@
 const path = require('path')
 
 module.exports = {
-  /* configureWebpack: {
-    resolve: {
-      alias: {
-        'Styles': path.resolve(__dirname, 'src/styles')
-      }
-    }
-  } */
   chainWebpack: (config) => {
     config.resolve.alias.set('Styles', path.resolve(__dirname, 'src/styles'))
   }
