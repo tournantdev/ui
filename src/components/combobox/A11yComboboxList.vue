dbb10a50fcc858e89070f35919dddf48ee52c6eb
Oscar <oscar.braunert@kitchenstories.de>
2018-09-14 04:10:50 +0200

    add list behavior

--- src/components/combobox/A11yComboboxList.vue
+++ src/components/combobox/A11yComboboxList.vue
@@ -1,14 +1,31 @@
 <template>
-  <ul id="vCbResultList">
+  <transition-group
+    name="list"
+    mode="out-in"
+    tag="ul"
+    id="vCbResultList"
+    :class="listClasses.listClass"
+    tabindex="0"
+  >
     <li
+      v-if="!items.length"
+      key="vCbNoResults"
+    >
+      <span>Sorry, but no results could be found …</span>
+    </li>
+    <li
+      v-else
       v-for="(item, index) in items"
       :key="index"
-      :id="`vCbItem-${index}`"
+      @click="$emit('resultClick', index)"
+      :id="getId(index)"
+      :class="listClasses.itemClass"
+      :aria-selected="(activeItem === index).toString()"
       role="option"
     >
-      {{ item }}
+      <span>{{ item.title }}</span>
     </li>
-  </ul>
+  </transition-group>
 </template>
 
 <script>
@@ -17,7 +34,55 @@ export default {
     items: {
       type: Array,
       default: () => []
+    },
+    activeItem: {
+      type: Number,
+      default: -1
     }
-  }
+  },
+  computed: {
+    listClasses () {
+      return this.isStyled
+        ? {
+          listClass: 'v-a11y-combobox__list',
+          itemClass: 'v-a11y-combobox__list-item'
+        }
+        : ''
+    }
+  },
+  inject: [
+    'isStyled',
+    'getId'
+  ]
 }
 </script>
+
+<style lang="scss">
+@import 'Styles/a11y-combobox-list.scss';
+
+.list-enter-active, .list-leave-active, .list-move {
+  transition: all .2s;
+}
+
+.list-enter {
+  transform: translateY(30px);
+}
+
+.list-enter,
+.list-leave-to  {
+  opacity: 0;
+}
+
+.list-leave-to {
+  position: absolute;
+}
+
+.v-a11y-combobox__list {
+  @include v-a11y-combobox__list;
+}
+
+.v-a11y-combobox__list-item {
+  @include v-a11y-combobox__list-item;
+}
+
+</style>
