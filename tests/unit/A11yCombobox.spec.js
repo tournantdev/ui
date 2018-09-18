3fd3b461aad1dfd6922283ac36e9e1b3cdb6517c
Oscar <o@ovl.design>
2018-09-18 09:23:04 +0200

    write unit tests

--- tests/unit/A11yCombobox.spec.js
+++ tests/unit/A11yCombobox.spec.js
@@ -1,21 +1,67 @@
 import { shallowMount } from '@vue/test-utils'
 import A11yCombox from '@/components/combobox/A11yCombobox.vue'
 
+const items = [
+  { id: 1, title: 'test title 2' },
+  { id: 2, title: 'test title 2' },
+  { id: 3, title: 'test title 3' },
+  { id: 4, title: 'test title 4' }
+]
+const inputLabel = 'Search for item'
+const descendantIdBase = 'vCbItem_'
+
 describe('A11yCombobox.vue', () => {
-  const inputLabel = 'Search for item'
-  const wrapper = shallowMount(A11yCombox, {
-    propsData: {
-      inputLabel
-    }
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(A11yCombox, {
+      propsData: {
+        items,
+        inputLabel
+      }
+    })
   })
 
   it('has a required inputLabel', () => {
     expect(wrapper.vm.$options.props.inputLabel.required).toBeTruthy()
   })
 
-  it('sets the correct aria-label', () => {
-    const $input = wrapper.vm.$el.querySelector('input')
+  it('has an input with required aria attributes', () => {
+    const $input = wrapper.find('input')
+    const inputAttr = $input.attributes()
 
-    expect($input.getAttribute('aria-label')).toBe(inputLabel)
+    expect(inputAttr['aria-autocomplete']).toBe('list')
+    expect(inputAttr['role']).toBe('searchbox')
+  })
+
+  it('sets the correct aria-label', () => {
+    const $input = wrapper.find('input')
+
+    expect($input.attributes()['aria-label']).toBe(inputLabel)
+  })
+
+  it('has a method to define the id of a list item', () => {
+    expect(wrapper.vm.getId).toBeDefined()
+    expect(wrapper.vm.getId(1)).toBe(`${descendantIdBase}1`)
+  })
+
+  it('starts at mouse position of `-1', () => {
+    console.log(wrapper.vm)
+    expect(wrapper.vm.$data.mousePosition).toBe(-1)
+  })
+
+  it('has no active descendant by default', () => {
+    const $input = wrapper.find('input')
+    const inputAttr = $input.attributes()
+
+    expect(inputAttr['aria-activedescendant']).toBe('')
+  })
+
+  it('updates `aria-activedescendant` based on arrowPosition', () => {
+    wrapper.setData({ arrowPosition: 1 })
+    const $input = wrapper.find('input')
+    const inputAttr = $input.attributes()
+
+    expect(inputAttr['aria-activedescendant']).toBe(`${descendantIdBase}1`)
   })
 })
