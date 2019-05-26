ef6b29bdf0a95174ea8fa0d1bc4ea0a2c93d3a7e
Oscar <o@ovl.design>
2019-05-26 20:50:49 +0200

    complete demo implementation

--- demo/App.vue
+++ demo/App.vue
@@ -1,11 +1,20 @@
 <template>
   <div id="app">
-    <vue-a11y-input v-model="input" label="Say hello" />
+    <vue-a11y-input
+      v-model="password"
+      :required="true"
+      :validation="$v.password"
+      label="Your password"
+      type="password"
+      name="password"
+    />
   </div>
 </template>
 
 <script>
 import VueA11yInput from '@/VueA11yInput.vue'
+
+import { required, minLength } from 'vuelidate/lib/validators'
 
 export default {
   name: 'App',
@@ -14,7 +23,13 @@ export default {
   },
   data() {
     return {
-      input: 'value'
+      password: ''
+    }
+  },
+  validations: {
+    password: {
+      required,
+      minLength: minLength(8)
     }
   }
 }
