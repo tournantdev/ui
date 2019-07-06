46db7ea48287f65152ae9b87512b428f100a410c
Oscar <o@ovl.design>
2019-07-06 10:13:17 +0200

    disable copy webpack

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
 	outputDir: path.resolve(__dirname, 'ui/dist'),
 	configureWebpack: {
-		plugins: [
+		/* plugins: [
 			new CopyWebpackPlugin([
 				{
 					from: path.join(__dirname, 'ui/public'),
@@ -12,7 +12,7 @@ module.exports = {
 					ignore: ['index.html', '.DS_Store']
 				}
 			])
-		]
+		] */
 	},
 	chainWebpack: config => {
 		config.plugin('html').tap(args => {
