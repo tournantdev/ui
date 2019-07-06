697fd7801d5a084c760c58cd89b703c7371c7227
Oscar <o@ovl.design>
2019-07-06 11:22:12 +0200

    🙈 update unit tests

---
+++ packages/combobox/tests/unit/Combobox.spec.js
@@ -0,0 +1,188 @@
+import { shallowMount } from '@vue/test-utils'
+import Combobox from '@p/combobox/src/index.vue'
+
+const items = [
+	{ id: 1, title: 'test title 2' },
+	{ id: 2, title: 'test title 2' },
+	{ id: 3, title: 'test title 3' },
+	{ id: 4, title: 'test title 4' }
+]
+const inputLabel = 'Search for item'
+const noResultsMessage = 'Sorry, no results'
+const descendantIdBase = 'vCbItem_'
+
+describe('Combobox.vue', () => {
+	let wrapper
+
+	beforeEach(() => {
+		wrapper = shallowMount(Combobox, {
+			propsData: {
+				items,
+				inputLabel,
+				noResultsMessage
+			},
+			attachToDocument: true
+		})
+	})
+
+	it('has a required inputLabel', () => {
+		expect(wrapper.vm.$options.props.inputLabel.required).toBeTruthy()
+	})
+
+	it('has a required noResultsMessage prop', () => {
+		expect(wrapper.vm.$options.props.noResultsMessage.required).toBeTruthy()
+	})
+
+	it('has an input with required aria attributes', () => {
+		const $input = wrapper.find('input')
+		const inputAttr = $input.attributes()
+
+		expect(inputAttr['aria-autocomplete']).toBe('list')
+		expect(inputAttr['role']).toBe('searchbox')
+	})
+
+	it('sets the correct aria-label', () => {
+		const $label = wrapper.find('label')
+
+		expect($label.text()).toBe(inputLabel)
+	})
+
+	it('has a method to define the id of a list item', () => {
+		expect(wrapper.vm.getId).toBeDefined()
+	})
+
+	it('creates the correct id', () => {
+		expect(wrapper.vm.getId(1)).toBe(`${descendantIdBase}1`)
+	})
+
+	it('starts at arrow position of `-1', () => {
+		expect(wrapper.vm.$data.arrowPosition).toBe(-1)
+	})
+
+	it('has no active descendant by default', () => {
+		const $input = wrapper.find('input')
+		const inputAttr = $input.attributes()
+
+		expect(inputAttr['aria-activedescendant']).toBeUndefined()
+	})
+
+	it('updates `aria-activedescendant` based on arrowPosition', () => {
+		const arrowPosition = 1
+		wrapper.setData({ arrowPosition })
+		const $input = wrapper.find('input')
+		const inputAttr = $input.attributes()
+
+		expect(inputAttr['aria-activedescendant']).toBe(
+			`${descendantIdBase}${arrowPosition}`
+		)
+	})
+
+	describe('arrow keys', () => {
+		it('calls onKeyDown if pressing the down arrow key', () => {
+			const stub = jest.fn()
+			const $input = wrapper.find('input')
+
+			wrapper.setMethods({ onKeyDown: stub })
+
+			$input.trigger('keyup.down')
+
+			expect(wrapper.vm.onKeyDown).toBeCalled()
+		})
+
+		it('calls onKeyUp if pressing the up arrow key', () => {
+			const stub = jest.fn()
+			const $input = wrapper.find('input')
+
+			wrapper.setMethods({ onKeyUp: stub })
+
+			$input.trigger('keyup.up')
+
+			expect(wrapper.vm.onKeyUp).toBeCalled()
+		})
+
+		it('changes nothing when pressing arrow keys if the list is hidden (default state)', () => {
+			wrapper.vm.onKeyDown()
+			expect(wrapper.vm.arrowPosition).toBe(-1)
+			wrapper.vm.onKeyUp()
+			expect(wrapper.vm.arrowPosition).toBe(-1)
+		})
+
+		it('increments the value if the list is visible', () => {
+			wrapper.setData({ inputValue: 'sa', hasFocus: true, arrowPosition: -1 })
+			wrapper.vm.onKeyDown()
+
+			expect(wrapper.vm.arrowPosition).toBe(0)
+		})
+
+		it('does not increment the value further than the length of the list', () => {
+			wrapper.setData({
+				inputValue: 'sa',
+				hasFocus: true,
+				arrowPosition: items.length
+			})
+			wrapper.vm.onKeyDown()
+
+			expect(wrapper.vm.arrowPosition).toBe(items.length)
+		})
+
+		it('decrements the value if the list is visible', () => {
+			wrapper.setData({ inputValue: 'sa', hasFocus: true, arrowPosition: 2 })
+			wrapper.vm.onKeyUp()
+
+			expect(wrapper.vm.arrowPosition).toBe(1)
+		})
+
+		it('activates the last item in the list if the beginning is reached', () => {
+			wrapper.setData({ inputValue: 'sa', hasFocus: true, arrowPosition: -1 })
+			wrapper.vm.onKeyUp()
+
+			expect(wrapper.vm.arrowPosition).toBe(items.length - 1)
+		})
+	})
+
+	describe('enter key', () => {
+		it('calls onEnter', () => {
+			const stub = jest.fn()
+			const $input = wrapper.find('input')
+
+			wrapper.setMethods({ onEnter: stub })
+			$input.trigger('keyup.enter')
+
+			expect(wrapper.vm.onEnter).toBeCalled()
+		})
+
+		it('sets the input value to the title of the currently selected item', () => {
+			const $input = wrapper.find('input')
+			wrapper.setData({ arrowPosition: 1 })
+			$input.trigger('keyup.enter')
+
+			expect(wrapper.vm.inputValue).toBe(items[1].title)
+		})
+	})
+
+	describe('escape key', () => {
+		it('calls onEscape', () => {
+			const stub = jest.fn()
+			const $input = wrapper.find('input')
+
+			wrapper.setMethods({ onEscape: stub })
+			$input.trigger('keyup.esc')
+
+			expect(wrapper.vm.onEscape).toBeCalled()
+		})
+
+		it('clears the input data', () => {
+			wrapper.setData({ inputValue: 'test' })
+			wrapper.vm.onEscape()
+
+			expect(wrapper.vm.inputValue).toBe('')
+		})
+
+		it('resets the arrow position', () => {
+			wrapper.setData({ inputValue: 'test' })
+			wrapper.vm.onEscape()
+
+			expect(wrapper.vm.arrowPosition).toBe(-1)
+		})
+	})
+})
---
+++ packages/input/tests/unit/Input.spec.js
@@ -0,0 +1,171 @@
+import { shallowMount } from '@vue/test-utils'
+
+import Input from '@p/input/src/index.vue'
+
+const defaultProps = {
+	label: 'Test label',
+	value: 'Say hello',
+	type: 'password',
+	validation: {
+		$error: true,
+		$dirty: false
+	},
+	required: false
+}
+
+describe('Input', () => {
+	let wrapper
+
+	beforeEach(() => {
+		wrapper = shallowMount(Input, {
+			propsData: defaultProps,
+			slots: {
+				'label-text': ' required',
+				feedback: '<p>Input is required</p>'
+			}
+		})
+	})
+
+	describe('props', () => {
+		it('has a required label prop', () => {
+			expect(wrapper.vm.$options.props.label.required).toBeTruthy()
+		})
+
+		it('renders the `required` attribute if `required` prop is set', () => {
+			wrapper.setProps({ required: true })
+
+			const input = wrapper.find('input')
+
+			expect(input.attributes('required')).toBeDefined()
+		})
+
+		it('has a validation prop', () => {
+			expect(wrapper.vm.$options.props.validation).toBeDefined()
+		})
+	})
+
+	it('links the label to the input', () => {
+		const label = wrapper.find('label')
+		const input = wrapper.find('input')
+		const id = input.attributes('id')
+		const labelFor = label.attributes('for')
+
+		expect(labelFor).toBe(id)
+	})
+
+	it('sets the inputType on the input element', () => {
+		const input = wrapper.find('input')
+		const type = input.attributes('type')
+
+		expect(type).toBe('password')
+	})
+
+	describe('`v-model` compatibility', () => {
+		it('sets the value prop on the input', () => {
+			const $input = wrapper.find('input')
+
+			expect($input.element.value).toBe('Say hello')
+		})
+
+		it('emits the value on change', () => {
+			const $input = wrapper.find('input')
+			$input.setValue('Wave goodbye')
+			const emittedInput = wrapper.emitted().input
+
+			expect(emittedInput).toBeTruthy()
+			expect(emittedInput[0]).toEqual(['Wave goodbye'])
+		})
+	})
+
+	describe('aria attributes invalid state', () => {
+		beforeEach(() => {
+			wrapper.setProps({
+				validation: {
+					$error: true,
+					$dirty: true
+				}
+			})
+		})
+
+		it('sets `aria-invalid`', () => {
+			const input = wrapper.find('input')
+
+			expect(input.attributes('aria-invalid')).toBe('true')
+		})
+
+		it('sets `aria-describedby` to the id of the feedback message container', () => {
+			const input = wrapper.find('input')
+			const feedbackMessages = wrapper.find('.v-a11y-input__feedback')
+			const feedbackId = feedbackMessages.attributes('id')
+
+			expect(input.attributes('aria-describedby')).toBe(feedbackId)
+		})
+	})
+
+	describe('input description', () => {
+		it('has the ability to set a description', () => {
+			expect(wrapper.vm.$options.props.description).toBeDefined()
+		})
+
+		it('links the description in `aria-describedby` if set', () => {
+			wrapper.setProps({
+				description: 'This is a description',
+				validation: { $error: false }
+			})
+
+			const input = wrapper.find('input')
+			const descriptionMessage = wrapper.find('.v-a11y-input__description')
+			const id = descriptionMessage.attributes('id')
+
+			expect(input.attributes('aria-describedby')).toBe(id)
+		})
+
+		it('links to description and feedback message if an error is present', () => {
+			wrapper.setProps({
+				description: 'This is a description',
+				validation: { $error: true }
+			})
+
+			const input = wrapper.find('input')
+			const descriptionMessage = wrapper.find('.v-a11y-input__description')
+			const feedbackMessages = wrapper.find('.v-a11y-input__feedback')
+			const feedbackId = feedbackMessages.attributes('id')
+			const descId = descriptionMessage.attributes('id')
+
+			expect(input.attributes('aria-describedby')).toBe(
+				`${feedbackId} ${descId}`
+			)
+		})
+	})
+
+	describe('slots', () => {
+		it('renders label text', () => {
+			wrapper.setProps({ required: true })
+
+			const label = wrapper.find('label')
+			const labelText = label.text()
+
+			expect(labelText.includes('required')).toBeTruthy()
+		})
+
+		it('hides the input messages if valid', () => {
+			const feedbackMessages = wrapper.find('.v-a11y-input__feedback')
+
+			wrapper.setProps({ validation: { $error: false, $dirty: true } })
+
+			expect(feedbackMessages.isVisible()).toBeFalsy()
+		})
+
+		it('renders feedback messages if value is invalid', () => {
+			const feedbackMessages = wrapper.find('.v-a11y-input__feedback')
+
+			wrapper.setProps({ validation: { $error: true, $dirty: true } })
+
+			expect(feedbackMessages.contains('p')).toBeTruthy()
+		})
+	})
+
+	it.skip('matches html structure', () => {
+		expect(wrapper.vm.$el).toMatchSnapshot()
+	})
+})
