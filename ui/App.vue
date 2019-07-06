28409e957b3ebf71d553433493652c76c66bf18d
Oscar <o@ovl.design>
2019-07-06 09:54:42 +0200

    move files for website into ui folder

---
+++ ui/App.vue
@@ -0,0 +1,44 @@
+<template>
+	<div id="app">
+		<h1>This is working.</h1>
+	</div>
+</template>
+
+<script>
+import VueA11yInput from '@/VueA11yInput.vue'
+
+import { required, minLength } from 'vuelidate/lib/validators'
+
+export default {
+	name: 'App',
+	components: {
+		VueA11yInput
+	},
+	data() {
+		return {
+			password: ''
+		}
+	},
+	validations: {
+		password: {
+			required,
+			minLength: minLength(8)
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+
+.v-a11y-input__label {
+	display: block;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,31 @@
 const path = require('path')
 
 module.exports = {
-  outputDir: path.resolve(__dirname, 'dist-demo'),
-  chainWebpack: config => {
-    config
-      .entry('app')
-      .clear()
-      .add('./demo/main.js')
-    config.resolve.alias
-      .set('Styles', path.resolve(__dirname, 'src/styles'))
-      .set('Demo', path.resolve(__dirname, 'demo'))
-  }
+	outputDir: path.resolve(__dirname, 'ui/dist'),
+	configureWebpack: {
+		plugins: [
+			new CopyWebpackPlugin([
+				{
+					from: path.join(__dirname, 'ui/public'),
+					to: path.join(__dirname, 'ui/dist'),
+					toType: 'dir',
+					ignore: ['index.html', '.DS_Store']
+				}
+			])
+		]
+	},
+	chainWebpack: config => {
+		config.plugin('html').tap(args => {
+			args[0].template = path.join(__dirname, 'ui/public/index.html')
+			return args
+		})
+		config
+			.entry('app')
+			.clear()
+			.add('./ui/src/main.js')
+		config.resolve.alias
+			.set('@p', path.resolve(__dirname, 'packages'))
+			.set('Styles', path.resolve(__dirname, 'src/styles'))
+			.set('Demo', path.resolve(__dirname, 'demo'))
+	}
 }
