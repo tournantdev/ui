8fe5ad5ec41c6cb1dd3e5860ce8151f2aaf67b1a
Oscar <o@ovl.design>
2019-07-08 20:55:33 +0200

    feature(dropdown): add component

---
+++ packages/dropdown/src/index.vue
@@ -0,0 +1,136 @@
+<template>
+	<div v-clickaway="close" class="t-ui-dropdown-menu">
+		<button
+			ref="toggle"
+			:aria-label="'Dropdown Menu'"
+			:aria-expanded="isVisible.toString()"
+			aria-haspopup="true"
+			@click="onClick"
+			@keyup.down="open"
+			@keyup.up="close"
+		>
+			<slot name="button-text" />
+		</button>
+		<transition name="fade-up">
+			<div
+				v-if="isVisible"
+				ref="menu"
+				role="menu"
+				class="t-ui-dropdown-menu__dropdown"
+				@keyup.down="onDownArrowPress"
+				@keyup.up="onUpArrowPress"
+				@keyup.esc="close"
+			>
+				<slot name="buttons" />
+			</div>
+		</transition>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'TournantDropdown',
+	components: {},
+	data() {
+		return {
+			isVisible: false,
+			buttons: [],
+			index: 0
+		}
+	},
+	beforeDestroy() {
+		if (this.buttons && this.buttons.length) {
+			this.buttons.forEach(button => {
+				button.removeEventListener('click', this.onMenuButtonClick)
+			})
+		}
+	},
+	methods: {
+		onClick() {
+			if (this.isVisible) {
+				this.close()
+			} else {
+				this.open()
+			}
+		},
+		onMenuButtonClick() {
+			this.close()
+		},
+		async open() {
+			this.isVisible = true
+
+			await this.$nextTick()
+
+			if (this.$refs.menu) {
+				this.buttons = Array.from(
+					this.$refs.menu.querySelectorAll(':not([disabled])')
+				)
+
+				this.buttons.forEach(button => {
+					button.addEventListener('click', this.onMenuButtonClick)
+				})
+
+				this.buttons[0].focus()
+			}
+		},
+		close() {
+			// Method will be called from the `clickaway` directive on every component instance
+			// Limit work and ensure correct handling of focus by having an additional check for visibility
+			if (this.isVisible) {
+				this.isVisible = false
+				this.buttons = undefined
+				this.index = 0
+				this.$refs.toggle.focus()
+			}
+		},
+		onDownArrowPress(e) {
+			e.preventDefault()
+			if (this.index < this.buttons.length - 1) {
+				this.index++
+			} else {
+				this.index = 0
+			}
+			this.buttons[this.index].focus()
+		},
+		onUpArrowPress(e) {
+			e.preventDefault()
+			console.log(e)
+			if (this.index === 0) {
+				this.index = this.buttons.length - 1
+			} else {
+				this.index--
+			}
+			this.buttons[this.index].focus()
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+.t-ui-dropdown-menu {
+	display: block;
+	z-index: 50;
+}
+
+.t-ui-dropdown-menu__dropdown {
+	background-color: white;
+	border-radius: 0.25rem;
+	box-shadow: 2px 4px 6px rgba(142, 142, 142, 0.4);
+	padding: space(half, relative) space(full, relative);
+	position: absolute;
+	right: 0;
+	text-align: left;
+	z-index: 100;
+
+	& > button {
+		display: block;
+		text-align: left;
+		white-space: nowrap;
+		width: 100%;
+
+		&:not(:last-child) {
+			margin-bottom: space(half, relative);
+		}
+	}
+}
+</style>
