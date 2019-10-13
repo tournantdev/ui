45f27b41ae9a74e4a0a536fa1b5ac3b0a9ab914b
Oscar <o@ovl.design>
2019-10-13 21:00:44 +0200

    test: add roots config

--- packages/input/jest.config.js
+++ packages/input/jest.config.js
@@ -4,8 +4,11 @@ const { name } = require('./package')
 // Package name is scoped to @tournant org, split @tournant/package-name for use in path matcher
 const folderName = name.split('/')[1]
 
+console.log(folderName)
+
 module.exports = {
 	...base,
+	roots: [`<rootDir>/packages/${folderName}`],
 	displayName: name,
 	name,
 	rootDir: '../..',
