97f5470aa32bb41723e1a16345c673c3a94b3cac
Oscar <o@ovl.design>
2019-10-13 20:48:14 +0200

    test: add module path to package configs

--- packages/combobox/jest.config.js
+++ packages/combobox/jest.config.js
@@ -2,11 +2,12 @@ const base = require('../../jest.config.base.js')
 const { name } = require('./package')
 
 // Package name is scoped to @tournant org, split @tournant/package-name for use in path matcher
-const path = name.split('/')[1]
+const folderName = name.split('/')[1]
 
 module.exports = {
 	...base,
 	displayName: name,
-	name: name,
-	rootDir: '../..'
+	name,
+	rootDir: '../..',
+	modulePaths: [`<rootDir>/packages/${folderName}`]
 }
