75290636c1b5ff2c8616781df1246cba4fb96688
Oscar <o@ovl.design>
2019-11-30 00:19:17 +0100

    feat(storybook): add example stories for dropdown

---
+++ packages/dropdown/dropdown.stories.js
@@ -0,0 +1,29 @@
+// import Vue from 'vue'
+import TournantDropdown from './src/index.vue'
+
+export default { title: 'Dropdown' }
+
+const items = `<template v-slot:items>
+			<button role="menuitem" tabindex="-1">Rename</button>
+			<button role="menuitem" tabindex="-1">Delete</button>
+		</template>`
+
+export const Basic = () => ({
+	components: { TournantDropdown },
+	template: `<tournant-dropdown >
+		<template v-slot:button-text>
+			Toggle
+		</template>
+		${items}
+	</tournant-dropdown>`
+})
+
+export const Positioning = () => ({
+	components: { TournantDropdown },
+	template: `<tournant-dropdown x-position="right" >
+		<template v-slot:button-text>
+			Toggle
+		</template>
+		${items}
+	</tournant-dropdown>`
+})
