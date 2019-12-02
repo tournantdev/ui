258083d20a708045d5adc40e927ac7ba8873a6ee
Oscar <o@ovl.design>
2019-12-02 19:44:32 +0100

    feat(alert): add stories for basic usage and long time out

--- packages/alert/tests/alert.stories.js
+++ packages/alert/tests/alert.stories.js
@@ -1,8 +1,46 @@
 import TournantAlert from '../src/index.vue'
+import { withKnobs, text } from '@storybook/addon-knobs'
 
-export default { title: '@tournant/alert' }
+export default { title: '@tournant/alert', decorators: [withKnobs] }
 
 export const basicExample = () => ({
 	components: { TournantAlert },
-	template: `<tournant-alert />`
+	props: {
+		alertMessage: {
+			default: text('Alert message', 'This is an alert message.')
+		}
+	},
+	data: () => ({
+		messages: []
+	}),
+	methods: {
+		addAlert() {
+			this.messages.push(this.alertMessage)
+		}
+	},
+	template: `<div><div>
+		<button @click="addAlert">Add alert</button>
+	</div>
+		<tournant-alert v-for="message in messages" :message="message" /></div>`
+})
+
+export const longTimeout = () => ({
+	components: { TournantAlert },
+	props: {
+		alertMessage: {
+			default: text('Alert message', 'This is an alert message.')
+		}
+	},
+	data: () => ({
+		messages: []
+	}),
+	methods: {
+		addAlert() {
+			this.messages.push(this.alertMessage)
+		}
+	},
+	template: `<div><div>
+		<button @click="addAlert">Add alert</button>
+	</div>
+		<tournant-alert v-for="message in messages" :message="message" :hideAfterSeconds="10" /></div>`
 })
